87c1f5d590a6e6449bfce61c5af8d0f24799e37a
Mauricio Laine <laine.mau@gmail.com>
2019-08-27 21:26:25 -0700

    added the recycler view dependency, and created a recycler view widget in the main layout

--- app/src/main/java/com/laine/mauro/minimalrv/MainActivity.kt
+++ app/src/main/java/com/laine/mauro/minimalrv/MainActivity.kt
@@ -2,11 +2,15 @@ package com.laine.mauro.minimalrv
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rv_names.visibility = View.VISIBLE
     }
 }
